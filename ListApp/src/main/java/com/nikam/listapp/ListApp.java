ff0cfcc14ec8e84374800ff5736f7b243c3d4fee
er-rahulnikam <errahulnikam@gmail.com>
2024-10-27 11:52:38 +0530

    Project initiated sucessfully!

---
+++ ListApp/src/main/java/com/nikam/listapp/ListApp.java
@@ -0,0 +1,13 @@
+package com.nikam.listapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListApp.class, args);
+	}
+
+}
